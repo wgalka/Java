96feefcef8d9fb1038aea7b57871aaf5b3057f81
wgalka <wgalka2@op.pl>
2022-11-08 15:07:55 +0100

    init

--- sourcecode/swing/src/main/java/Main2.java
+++ sourcecode/swing/src/main/java/Main2.java
@@ -1,10 +1,2 @@
-import javax.swing.*;
-
-public class Main2 {
-    public static void main(String[] args) {
-        Okno x = new Okno();
-        x.setContentPane(x.panel);
-        x.pack();
-        x.setVisible(true);
-    }
+package PACKAGE_NAME;public class Main2 {
 }
